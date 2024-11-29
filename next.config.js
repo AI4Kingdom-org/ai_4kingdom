807222a42f2cab62196f478f57e5f4aa680267f3
Hanmo Zhang <zhanghanmo888@gmail.com>
2024-11-29 06:44:25 -0800

    Clean up next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 const nextConfig = {
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY?.trim(),
     OPENAI_ORG_ID: process.env.OPENAI_ORG_ID?.trim(),
   }
 }
-=======
-const nextConfig = {}
->>>>>>> 44d991b40406b5ed12dbd3731740d81f976b7b04
 
 module.exports = nextConfig
