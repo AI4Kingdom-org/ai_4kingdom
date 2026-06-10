69975f4fd6fb190e40b68a1dc658673ad58cba6a
Samuel H.Y. Lin <hsianglin124@gmail.com>
2026-06-10 23:08:01 +0800

    fix: 修正手機版 chat UI 版面變形及 history 下拉選單問題與 placeholder text changed.

--- app/cfsc-church/navigator/page.tsx
+++ app/cfsc-church/navigator/page.tsx
@@ -69,6 +69,7 @@ function CfscChurchNavigatorContent() {
     setChatError('');
     setMessages([]);
     setCurrentThreadId(threadId);
+    setSidebarOpen(false);
   };
 
   const handleSendMessage = async (message: string) => {
